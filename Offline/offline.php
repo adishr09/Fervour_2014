132e11a51549333b7f8b83da5a197ce19c2f26f5
archangel <adi.shr09@gmail.com>
2014-03-29 21:48:09 +0530

    smthing new

--- Offline/offline.php
+++ Offline/offline.php
@@ -1,20 +1,10 @@
-<!DOCTYPE html>
-<html lang="en" class="no-js">
-	<head>
-		<meta charset="UTF-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
-		<meta name="viewport" content="width=device-width, initial-scale=1"> 
+<?php echo'
 		<title>Offline Events</title>
-		<meta name="description" content="3D Grid Effect: Recreation of the effect seen in the prototype app by Marcus Eckert | Demo 1" />
-		<meta name="keywords" content="3d, grid, effect, flip, css transform, perspective, web design" />
-		<meta name="author" content="Codrops" />
 		<link rel="shortcut icon" href="../favicon.ico">
-		<link rel="stylesheet" type="text/css" href="css/normalize.css" />
-		<link rel="stylesheet" type="text/css" href="css/demo.css" />
-		<link rel="stylesheet" type="text/css" href="css/component.css" />
-		<script src="js/modernizr.custom.js"></script>
-	</head>
-	<body>
+		<link rel="stylesheet" type="text/css" href="offline/css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="offline/css/demo.css" />
+		<link rel="stylesheet" type="text/css" href="offline/css/component.css" />
+		<script src="offline/js/modernizr.custom_1.js"></script>
 		<div class="container">
 			<!-- Top Navigation -->
 			<header class="codrops-header">
@@ -26,36 +16,36 @@
 			<section class="grid3d vertical" id="grid3d">
 				<div class="grid-wrap">
 					<div class="grid">
-						<figure><img src="img/1.jpg" alt="img01"/></figure>
-						<figure><img src="img/5.jpg" alt="img05"/></figure>
-						<figure><img src="img/8.jpg" alt="img08"/></figure>
-						<figure><img src="img/2.jpg" alt="img02"/></figure>
-						<figure><img src="img/4.jpg" alt="img04"/></figure>
-						<figure><img src="img/3.jpg" alt="img03"/></figure>
-						<figure><img src="img/9.jpg" alt="img09"/></figure>
-						<figure><img src="img/6.jpg" alt="img06"/></figure>
-						<figure><img src="img/7.jpg" alt="img07"/></figure>
-						<figure><img src="img/1.jpg" alt="img01"/></figure>
-						<figure><img src="img/5.jpg" alt="img05"/></figure>
-						<figure><img src="img/8.jpg" alt="img08"/></figure>
-						<figure><img src="img/2.jpg" alt="img02"/></figure>
-						<figure><img src="img/4.jpg" alt="img04"/></figure>
-						<figure><img src="img/3.jpg" alt="img03"/></figure>
-						<figure><img src="img/9.jpg" alt="img09"/></figure>
-						<figure><img src="img/6.jpg" alt="img06"/></figure>
+						<figure><img src="offline/img/1.jpg" alt="img01"/></figure>
+						<figure><img src="offline/img/5.jpg" alt="img05"/></figure>
+						<figure><img src="offline/img/8.jpg" alt="img08"/></figure>
+						<figure><img src="offline/img/2.jpg" alt="img02"/></figure>
+						<figure><img src="offline/img/4.jpg" alt="img04"/></figure>
+						<figure><img src="offline/img/3.jpg" alt="img03"/></figure>
+						<figure><img src="offline/img/9.jpg" alt="img09"/></figure>
+						<figure><img src="offline/img/6.jpg" alt="img06"/></figure>
+						<figure><img src="offline/img/7.jpg" alt="img07"/></figure>
+						<figure><img src="offline/img/1.jpg" alt="img01"/></figure>
+						<figure><img src="offline/img/5.jpg" alt="img05"/></figure>
+						<figure><img src="offline/img/8.jpg" alt="img08"/></figure>
+						<figure><img src="offline/img/2.jpg" alt="img02"/></figure>
+						<figure><img src="offline/img/4.jpg" alt="img04"/></figure>
+						<figure><img src="offline/img/3.jpg" alt="img03"/></figure>
+						<figure><img src="offline/img/9.jpg" alt="img09"/></figure>
+						<figure><img src="offline/img/6.jpg" alt="img06"/></figure>
 					</div>
 				</div><!-- /grid-wrap -->
 				<div class="content">
 					<div>
 						<div class="dummy-img"></div>
-                        <p class="dummy-head">BULLS' AND BEARS</p>
-						<p class="dummy-text">"Money was never a big motivation for me : except as a way to keep score. The real excitement is playing                            the game." - Donald J. Trump</p>
-						<p class="dummy-text">We've all been bowled over by 'The Wolf of Wall Street'...Calling out to all you Jordan Belforts...to                               participate in "BULLS N BEARS" a virtual Stock Marketing event . This event creates a chance for you to enter a mock share                          market and lets one trade and gain market experience without having to invest actual money. So If you have never known what a                       share is, and have only wondered how everybody buys shares and makes riches, here is just the game to help you get started. If                          the beguiling world of stocks fascinates you, if you have the nerve to compete with the ferocious wolves of Wall Street,                             then this is the place to be. So trade your stocks right away and be a billionaire though virtually.!</p>
+                        <p class="dummy-head">BULLS\' AND BEARS</p>
+						<p class="dummy-text">"Money was never a big motivation for me : except as a way to keep score. The real excitement is playing the game." - Donald J. Trump</p>
+						<p class="dummy-text">We\'ve all been bowled over by \'The Wolf of Wall Street\'...Calling out to all you Jordan Belforts...to participate in "BULLS N BEARS" a virtual Stock Marketing event . This event creates a chance for you to enter a mock share market and lets one trade and gain market experience without having to invest actual money. So If you have never known what a share is, and have only wondered how everybody buys shares and makes riches, here is just the game to help you get started. If the beguiling world of stocks fascinates you, if you have the nerve to compete with the ferocious wolves of Wall Street, then this is the place to be. So trade your stocks right away and be a billionaire though virtually.!</p>
 					</div>
 					<div>
 						<div class="dummy-img"></div>
                         <p class="dummy-head">CROOK THE BOOK</p>
-						<p class="dummy-text">An Event which will allow you to showcase your knowledge of seemingly unimportant things in C/C++ most                              people miss.</p>
+						<p class="dummy-text">An Event which will allow you to showcase your knowledge of seemingly unimportant things in C/C++ most people miss.</p>
 						<p class="dummy-text">The only people for me are the mad ones, the ones who are mad to live, mad to talk, mad to be saved, desirous of everything at the same time, the ones who never yawn or say a commonplace thing, but burn, burn, burn like fabulous yellow roman candles exploding like spiders across the stars.</p>
 					</div>
 					<div>
@@ -66,28 +56,26 @@
 					</div>
 					<div>
 						<div class="dummy-img"></div>
-                        <p class="dummy-head">JUST F'CKING DO IT </p>
-						<p class="dummy-text">Are you crazy, fast, bold and funny enough to complete the tasks we have for you? Then be here. Whatever comes your way, just f'cking do it man!
-</p>
+                        <p class="dummy-head">JUST F\'CKING DO IT </p>
+						<p class="dummy-text">Are you crazy, fast, bold and funny enough to complete the tasks we have for you? Then be here. Whatever comes your way, just f\'cking do it man!</p>
 						<p class="dummy-text">The only people for me are the mad ones, the ones who are mad to live, mad to talk, mad to be saved, desirous of everything at the same time, the ones who never yawn or say a commonplace thing, but burn, burn, burn like fabulous yellow roman candles exploding like spiders across the stars.</p>
 					</div>
 					<div>
 						<div class="dummy-img"></div>
                         <p class="dummy-head">SUMOBOTICS</p>
-                        <p class="dummy-text">Our sumobotics event is inspired by the ancient japanese martial art, from where it gets its name. If you feel that your robot has the strength to draw the opponent's bot out of the ring, this IS THE EVENT FOR YOU. </p>
+                        <p class="dummy-text">Our sumobotics event is inspired by the ancient japanese martial art, from where it gets its name. If you feel that your robot has the strength to draw the opponent\'s bot out of the ring, this IS THE EVENT FOR YOU. </p>
 						<p class="dummy-text">The only people for me are the mad ones, the ones who are mad to live, mad to talk, mad to be saved, desirous of everything at the same time, the ones who never yawn or say a commonplace thing, but burn, burn, burn like fabulous yellow roman candles exploding like spiders across the stars.</p>
 					</div>
 					<div>
 						<div class="dummy-img"></div>
                         <p class="dummy-head">JAM.comm</p>
-						<p class="dummy-text">Too fond of blabbering? Speak on the given topic for a minute and win. It's that simple, or maybe not. Blah Blah!</p>
+						<p class="dummy-text">Too fond of blabbering? Speak on the given topic for a minute and win. It\'s that simple, or maybe not. Blah Blah!</p>
 						<p class="dummy-text">The only people for me are the mad ones, the ones who are mad to live, mad to talk, mad to be saved, desirous of everything at the same time, the ones who never yawn or say a commonplace thing, but burn, burn, burn like fabulous yellow roman candles exploding like spiders across the stars.</p>
 					</div>
 					<div>
 						<div class="dummy-img"></div>
                         <p class="dummy-head">THE BRUSH BATTLE</p>
-						<p class="dummy-text">Seeking for a chance to portray your creative ideas?
-    So here is a platform for you where you can paint your ideas in a virtual world and also market the weird ones.</p>
+						<p class="dummy-text">Seeking for a chance to portray your creative ideas? So here is a platform for you where you can paint your ideas in a virtual world and also market the weird ones.</p>
 						<p class="dummy-text">The only people for me are the mad ones, the ones who are mad to live, mad to talk, mad to be saved, desirous of everything at the same time, the ones who never yawn or say a commonplace thing, but burn, burn, burn like fabulous yellow roman candles exploding like spiders across the stars.</p>
 					</div>
 					<div>
@@ -145,11 +133,10 @@
 				</div>
 			</section>
 					</div><!-- /container -->
-		<script src="js/classie.js"></script>
-		<script src="js/helper.js"></script>
-		<script src="js/grid3d.js"></script>
+		<script src="offline/js/classie.js"></script>
+		<script src="offline/js/helper.js"></script>
+		<script src="offline/js/grid3d.js"></script>
 		<script>
-			new grid3D( document.getElementById( 'grid3d' ) );
+			new grid3D( document.getElementById( \'grid3d\' ) );
 		</script>
-	</body>
-</html>
+		';?>
