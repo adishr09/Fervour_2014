2dd4cd5014bd479e38a7e60f28e51e3772770203
KSHITIJ KAUSHIK <kshkshi.kaushik@gmail.com>
2014-04-02 21:57:40 +0530

    DONE TEXT WORK

--- Offline/offline.php
+++ Offline/offline.php
@@ -46,86 +46,78 @@
 					</div>
 				</div><!-- /grid-wrap -->
 				<div class="content">
-					<div>
-						<div class="dummy-img"></div>
-                        <p class="dummy-head">BULLS' AND BEARS</p>
-						<p class="dummy-text">"Money was never a big motivation for me : except as a way to keep score. The real excitement is playing                            the game." - Donald J. Trump</p>
-						<p class="dummy-text">We've all been bowled over by 'The Wolf of Wall Street'...Calling out to all you Jordan Belforts...to                               participate in "BULLS N BEARS" a virtual Stock Marketing event . This event creates a chance for you to enter a mock share                          market and lets one trade and gain market experience without having to invest actual money. So If you have never known what a                       share is, and have only wondered how everybody buys shares and makes riches, here is just the game to help you get started. If                          the beguiling world of stocks fascinates you, if you have the nerve to compete with the ferocious wolves of Wall Street,                             then this is the place to be. So trade your stocks right away and be a billionaire though virtually.!</p>
-                        <br><br>
-                        <p class="dummy-text">RULES</p>
-                        <p class="dummy-text">EVENT MANAGERS<br>Aman Garg    Anshul Sharma</p>
-                        <p class="dummy-text">Venue: Auditorium</p>
-                        <p class="dummy-text">Time: 3:00 PM - 6:00 PM
-					   </div>
-					<div>
+				
+                    <div>
 						<div class="dummy-img"></div>
                         <p class="dummy-head">CROOK THE BOOK</p>
-						<p class="dummy-text">An Event which will allow you to showcase your knowledge of seemingly unimportant things in C/C++ most                              people miss.</p>
-				       <br><br>
+						<p class="dummy-text">An Event which will allow you to showcase your knowledge of seemingly unimportant things in C/C++ most                           people miss.</p>
+				        <br><br>
                         <p class="dummy-text">RULES</p>
                         <p class="dummy-text">EVENT MANAGERS<br>Sakshi Rungta   Neeraj Yadav</p>
                         <p class="dummy-text">Venue: B Block / A block Lab</p>
                         <p class="dummy-text">Time: 12:00 AM - 3:00 PM
-				
-					</div>
-					<div>
+				    </div>
+					
+                    <div>
 						<div class="dummy-img"></div>
 						<p class="dummy-head">FULL THROTTLE </p>
-                        <p class="dummy-text">Are you fast enough to be furious on your rivals? Are you an insuperable counter striker? Then BRING IT ON !! </p>
-				 <br><br>
-                        <p class="dummy-text">RULES</p>
+                        <p class="dummy-text">Are you fast enough to be furious on your rivals? Are you an insuperable counter striker? Then BRING IT                         ON !! </p>
+                        <br><br>
+                        <p class="dummy-text">RULES and REGISTRATION: <a href="https://www.facebook.com/events/1557345194491318/">Click Here</a></p>
                         <p class="dummy-text">EVENT MANAGERS<br>Sahil Suneja    Sanchit Guglani     Gopesh Dwivedi</p>
                         <p class="dummy-text">Venue: IT Labs</p>
                         <p class="dummy-text">Time: 11:00 AM - 7:00 PM
                     </div>
-					<div>
+					
+                    <div>
 						<div class="dummy-img"></div>
                         <p class="dummy-head">JUST DO IT </p>
-						<p class="dummy-text">Are you crazy, fast, bold and funny enough to complete the tasks we have for you? Then be here. Whatever comes your way, just do it man!
-</p>
-                        
-                         <br><br>
+						<p class="dummy-text">Are you crazy, fast, bold and funny enough to complete the tasks we have for you? Then be here. Whatever                         comes your way, just do it man!
+                        </p>
+                        <br><br>
                         <p class="dummy-text">RULES</p>
                         <p class="dummy-text">EVENT MANAGERS<br>Aman Jain   Aman Kaushik</p>
                         <p class="dummy-text">Venue: Hawa Lawn</p>
                         <p class="dummy-text">Time: 12:00 PM - 5:00 PM
-											</div>
-					<div>
+				    </div>
+					
+                    <div>
 						<div class="dummy-img"></div>
                         <p class="dummy-head">SUMOBOTICS</p>
-                        <p class="dummy-text">Our sumobotics event is inspired by the ancient japanese martial art, from where it gets its name. If you feel that your robot has the strength to draw the opponent's bot out of the ring, this IS THE EVENT FOR YOU. </p>
-                        
-                         <br><br>
+                        <p class="dummy-text">Our sumobotics event is inspired by the ancient japanese martial art, from where it gets its name. If                           you feel that your robot has the strength to draw the opponent's bot out of the ring, this IS THE EVENT FOR YOU. </p>
+                        <br><br>
                         <p class="dummy-text">RULES</p>
                         <p class="dummy-text">EVENT MANAGERS<br>Tapan Khattar   Shivam Bhardwaj     Pranshu Jambh</p>
                         <p class="dummy-text">Venue: Central Lawn</p>
                         <p class="dummy-text">Time: 11:00 AM - 5:00 PM
 					</div>
-					<div>
+				
+                    <div>
 						<div class="dummy-img"></div>
                         <p class="dummy-head">JAM.comm</p>
-						<p class="dummy-text">Too fond of blabbering? Speak on the given topic for a minute and win. It's that simple, or maybe not. Blah Blah!</p>
-                        
-                         <br><br>
+						<p class="dummy-text">Too fond of blabbering? Speak on the given topic for a minute and win. It's that simple, or maybe not.                           Blah Blah!</p>
+                        <br><br>
                         <p class="dummy-text">RULES</p>
                         <p class="dummy-text">EVENT MANAGERS<br>Aishwarya Kaul  Sumedha Arya</p>
                         <p class="dummy-text">Venue: ED Room</p>
                         <p class="dummy-text">Time: 11:00 AM - 1:00 PM
 					</div>
-					<div>
+				
+                    <div>
+                        
 						<div class="dummy-img"></div>
                         <p class="dummy-head">THE BRUSH BATTLE</p>
 						<p class="dummy-text">Seeking for a chance to portray your creative ideas?
-    So here is a platform for you where you can paint your ideas in a virtual world and also market the weird ones.</p>
-						
-                         <br><br>
+                        So here is a platform for you where you can paint your ideas in a virtual world and also market the weird ones.</p>
+						<br><br>
                         <p class="dummy-text">RULES</p>
                         <p class="dummy-text">EVENT MANAGERS<br>Samarth Soni    Mridul Miglani  Akshita Gupta</p>
                         <p class="dummy-text">Venue: Lawn Behind D Block Walls</p>
                         <p class="dummy-text">Time: 12:00 PM - 4:00 PM
 					</div>
-					<div>
-						<div class="dummy-img"></div>
+					
+                    <div>
+                        <div class="dummy-img"></div>
 						<p class="dummy-head">RUN FOR YOUR CODE</p>
 						<p class="dummy-text">articipant has to finish the given offline and online codes in minimum time period</p>
 						<br><br>
@@ -134,7 +126,8 @@
                         <p class="dummy-text">Venue: A Block Lab</p>
                         <p class="dummy-text">Time: 11:00 AM - 2:00 PM
 					</div>
-					<div>
+					
+                    <div>
 						<div class="dummy-img"></div>
                         <p class="dummy-head">BLIND CODING</p>
 						<p class="dummy-text">Type your code with the monitor turned off</p>
@@ -145,11 +138,12 @@
                         <p class="dummy-text">Time: 12:00 PM - 2:00 PM
                         
 					</div>
-					<div>
+					
+                    <div>
 						<div class="dummy-img"></div>
                         <p class="dummy-head">FOLLOW ME</p>
-						<p class="dummy-text">A line following robot is one which trails the line on the path it navigates. In this event the robots follow a white line on a black surface or vice versa (will depend on the level). The contest will have TWO ROUNDS. In the first round, the bots will be pitted in a race against eeach other, where the fastest bot wins. In the second round, the robot will have to navigate several turns at different angles and shapes. The robot which is able to navigate the track in shortest time will be declared the winner. 
-</p>
+						<p class="dummy-text">A line following robot is one which trails the line on the path it navigates. In this event the robots                           follow a white line on a black surface or vice versa (will depend on the level). The contest will have TWO ROUNDS. In the                             first round, the bots will be pitted in a race against eeach other, where the fastest bot wins. In the second round, the robot                         will have to navigate several turns at different angles and shapes. The robot which is able to navigate the track in shortest                         time will be declared the winner.     
+                        </p>
 						<br><br>
                         <p class="dummy-text">RULES:https://www.facebook.com/events/234838080052871/</p>
                         <p class="dummy-text">EVENT MANAGERS<br>Abhinav Sarkar   Siddhanth Gulhar Dhruv Gabba<br>Ph-8802865125   Ph-9810920546</p>
@@ -157,7 +151,8 @@
                         <p class="dummy-text">Time: 12:00 PM - 2:00 PM
                         
 					</div>
-					<div>
+					
+                    <div>
 						<div class="dummy-img"></div>
                         <p class="dummy-head">TRAILBLAZER</p>
 						<p class="dummy-text">Run your bot on a path with out of the blue obstacles</p>
@@ -168,10 +163,11 @@
                         <p class="dummy-text">Time: 11:00 AM - 2:00 PM
                         
 					</div>
+                    
 					<div>
 						<div class="dummy-img"></div>
                         <p class="dummy-head">Clash Of the Titans:Ultimate PS3/Xbox Gamers</p>
-						<p class="dummy-text">PS3 and Xbox gaming event comprising of knockout rounds followed by a Tripple Threat Match declaring the winner.</p>
+						<p class="dummy-text">PS3 and Xbox gaming event comprising of knockout rounds followed by a Tripple Threat Match declaring the                         winner.</p>
 						<br><br>
                         <p class="dummy-text">RULES:<br></p>
                         <p class="dummy-text">EVENT MANAGERS<br>Jagjot Singh Sehgal     Himanshu Aggarwal   Shivam Bindal</p>
@@ -179,7 +175,8 @@
                         <p class="dummy-text">Time: 11:00 AM - 5:00 PM
                         
 					</div>
-					<div>
+					
+                    <div>
 						<div class="dummy-img"></div>
                         <p class="dummy-head">MELA Quiz</p>
 						<p class="dummy-text"></p>
@@ -190,7 +187,8 @@
                         <p class="dummy-text">Time: 2:00 PM - 4:00 PM
                         
 					</div>
-					<div>
+					
+                    <div>
 						<div class="dummy-img"></div>
 						
                         <p class="dummy-head">Technical Quiz</p>
@@ -202,41 +200,76 @@
                         <p class="dummy-text">Time: 11:00 AM - 1:00 PM
                         
 					</div>
-					<div>
+					
+                    <div>
 						<div class="dummy-img"></div>
                         <p class="dummy-head">GOT THE AD-SENSE</p>
-						<p class="dummy-text">Ever thought about some weirdest products and felt that advertisements you see on tv could be much better?well you could be the next advertisement guru!! 
-Our PRODUCT, You THINK, You ENACT and make your own advertisement on the bizzare products.So! Its time to think new and innovative because NOTHING SELLS LIKE VIDEO!
-</p>
+						<p class="dummy-text">Ever thought about some weirdest products and felt that advertisements you see on tv could be much                               better?well you could be the next advertisement guru!!<br> 
+                        Our PRODUCT, You THINK, You ENACT and make your own advertisement on the bizzare products.So! Its time to think new and                               innovative because NOTHING SELLS LIKE VIDEO!
+                        </p>
 						<br><br>
                         <p class="dummy-text">RULES:<br>
-1.Max time for ad video is 2 mins.<br>
-2.Advertisement can be in English/Hindi or bilingual.<br>
-3.Participation in teams only. Maximum members allowed per team is 4.<br>
-4.Caution should be taken to refrain from displaying obscenity,violence etc.<br>
-5.In case video exceeds the time limit the team will be disqualified.<br>
-<br>
-The competition consists of only one round:<br>
-FIRST ROUND-Each team will be given one products name and after 15 mins they have to show us their advertisement video.
-
-INFRASTRUCTURE: space for registration and a classroom.
-</p>
+                            1.Max time for ad video is 2 mins.<br>
+                            2.Advertisement can be in English/Hindi or bilingual.<br>
+                            3.Participation in teams only. Maximum members allowed per team is 4.<br>
+                            4.Caution should be taken to refrain from displaying obscenity,violence etc.<br>
+                            5.In case video exceeds the time limit the team will be disqualified.<br>
+                            <br>
+                            The competition consists of only one round:<br>
+                            FIRST ROUND-Each team will be given one products name and after 15 mins they have to show us their advertisement video.
+                        </p>
                         <p class="dummy-text">EVENT MANAGERS<br>Arjun Juneja</p>
                         <p class="dummy-text">Venue: Seminar Hall</p>
                         <p class="dummy-text">Time: 11:00 AM - 1:00 PM
                         
 					</div>
-					<div>
+					
+                    <div>
 						<div class="dummy-img"></div>
-						<p class="dummy-text">The only people for me are the mad ones, the ones who are mad to live, mad to talk, mad to be saved, desirous of everything at the same time, the ones who never yawn or say a commonplace thing, but burn, burn, burn like fabulous yellow roman candles exploding like spiders across the stars.</p>
-						<p class="dummy-text">The only people for me are the mad ones, the ones who are mad to live, mad to talk, mad to be saved, desirous of everything at the same time, the ones who never yawn or say a commonplace thing, but burn, burn, burn like fabulous yellow roman candles exploding like spiders across the stars.</p>
-					</div>
-					<div>
+                        <p class="dummy-head">FUTSAL</p>
+						<p class="dummy-text">Mini football competition</p>
+						<br><br>
+                        <p class="dummy-text">RULES:<br></p>
+                        <p class="dummy-text">EVENT MANAGERS<br>Akshay Gupta Kunal Khatri Iknoor Singh Sandhu</p>
+                        <p class="dummy-text">Venue: Ground</p>
+                        <p class="dummy-text">Time: 10:00 AM - 7:00 PM
+                    
+                    </div>
+					
+                    <div>
 						<div class="dummy-img"></div>
-						<p class="dummy-text">The only people for me are the mad ones, the ones who are mad to live, mad to talk, mad to be saved, desirous of everything at the same time, the ones who never yawn or say a commonplace thing, but burn, burn, burn like fabulous yellow roman candles exploding like spiders across the stars.</p>
-						<p class="dummy-text">The only people for me are the mad ones, the ones who are mad to live, mad to talk, mad to be saved, desirous of everything at the same time, the ones who never yawn or say a commonplace thing, but burn, burn, burn like fabulous yellow roman candles exploding like spiders across the stars.</p>
-					</div>
-					<span class="loading"></span>
+                        <p class="dummy-head">I'M FEELING LCUKY</p>
+						<p class="dummy-text">Check your spontaneity as you answer questions in the rapid fire round. Best part? You can 'Google'                             them! Rack your brains in the second round to unlock clues! </p>
+						<br><br>
+                        <p class="dummy-text">RULES:<br></p>
+                        <p class="dummy-text">EVENT MANAGERS<br>Dharvi Verma Divyanshu Sharma Aditya Dev Sharma</p>
+                        <p class="dummy-text">Venue: ED ROOM</p>
+                        <p class="dummy-text">Time: 1:00 PM - 3:00 PM
+                    </div>
+					
+                    <div>
+						<div class="dummy-img"></div>
+                        <p class="dummy-head">BILLS AND BEARS: Mock Stock Exchange</p>
+						<p class="dummy-text">Enter a mock share market and trade to gain market experience without investing actual money.We've all                           been bowled over by 'The Wolf of Wall Street'. Calling out to all you Jordan Belforts to participate in "BULLS N BEARS" a                             virtual Stock Marketing event. So trade your stocks right away and be a billionaire</p>
+						<br><br>
+                        <p class="dummy-text">RULES:<br></p>
+                        <p class="dummy-text">EVENT MANAGERS<br>Aman Garg Anshul Sharma</p>
+                        <p class="dummy-text">Venue: Auditorium</p>
+                        <p class="dummy-text">Time: 3:00 PM - 6:00 PM
+                    </div>
+                    
+                    <div>
+						<div class="dummy-img"></div>
+                        <p class="dummy-head">NSS</p>
+						<p class="dummy-text">Technical Paper Presenation</p>
+						<br><br>
+                        <p class="dummy-text">RULES:<br></p>
+                        <p class="dummy-text">EVENT MANAGERS<br>Vijayita Gumber Priyanka Sharma Eshita Aggarwal</p>
+                        <p class="dummy-text">Venue: Seminar Hall</p>
+                        <p class="dummy-text">Time: 1:00 PM - 5:00 PM
+                    </div>
+                    
+                    <span class="loading"></span>
 					<span class="icon close-content"></span>
 				</div>
 			</section>
