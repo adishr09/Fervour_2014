435277b6697ec393004329ae738ee2042c3b6854
nonson <priyanshsingh@outlook.com>
2014-04-12 02:19:41 +0530

    php

---
+++ index (2).php
@@ -0,0 +1,50 @@
+<?php
+$con=mysqli_connect("localhost","root","","data");
+// Check connection
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+$j = 5;
+$k = "SSS";
+$l = 3;
+$sql="INSERT INTO test (uid, name, level)
+VALUES ('$j', '$k', '$l')";
+
+if (!mysqli_query($con,$sql))
+{
+  die('Error: ' . mysqli_error($con));
+}
+echo "1 record added";
+
+$result = mysqli_query($con,"SELECT * FROM test");
+
+echo "<table border='1'>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+<th>level</th>
+
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+  {
+  echo "<tr>";
+  echo "<td>" . $row['uid'] . "</td>";
+  echo "<td>" . $row['name'] . "</td>";
+      echo "<td>" . $row['level'] . "</td>";
+
+  echo "</tr>";
+  }
+echo "</table>";
+
+$result = mysqli_query($con,"SELECT * FROM test
+WHERE name='sss'");
+
+while($row = mysqli_fetch_array($result))
+  {
+  echo $row['name'] . " " . $row['level'];
+  echo "<br>";
+  }
+mysqli_close($con);
+?>
