5182d97aee19fab02dd56b708e50ce116b998503
archangel <adi.shr09@gmail.com>
2014-04-12 14:06:19 +0530

    some minor changes

---
+++ Online/Events/instaedit.php
@@ -0,0 +1,32 @@
+<html style="overflow-x:hidden;">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1"> 
+		<title>INSTAEDIT - Online Fervour 2014</title>
+		<link rel="stylesheet" href="css/body.css" type="text/css">
+	</head>
+<div id="wrapper" >
+	<div id="container">
+		<div id="left">
+			<p>We invite all shutterbugs to "InstaEdit :Photography For Perfection" event as a part of online
+				fervour. All photographers get ready with your camera as you will be
+				awarded for sharing your best moments of the day via photos, whether
+				its a coke with friends or candid moment with your crush. Express
+				yourself and stand a chance to win prizes.
+				Theme-"DOWN THE MEMORY LANE"
+				General guidelines-
+				-join the event page
+				-mail your pics-arshiyapeer94@gmail.com
+				-get your friends to like the pic
+				The winner will be on the basis of likes on the pic plus our assessment
+				So brace youself for the competition.</p>
+		</div>
+		<div id="border">
+		</div>
+		<div id="right">
+			<p>This is 2nd right</p>
+		</div>
+	</div>
+</div>
+</html>
---
+++ Online/Events/web_design.php
@@ -0,0 +1,37 @@
+<html style="overflow-x:hidden;">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1"> 
+		<title>DESIGN THE WEB - Online Fervour 2014</title>
+		<link rel="stylesheet" href="css/body.css" type="text/css">
+	</head>
+<div id="wrapper" >
+	<div id="container">
+		<div id="left">
+			<p>Do you have a knack for Web Designing? Do you think you can design the best User Interface ? Design a webpage on the following theme and win great prizes.
+
+Theme: "Virtual World"
+
+Rules: All platforms for the website are allowed.
+STRICTLY! No CMS and No copied themes.
+
+How to Submit: Send us the git repository, the zip file or link to where website is hosted at kunsachdeva@gmail.com with subject title : "Web Design 2014". Also mail a small description of what you have created and your contact details.
+
+Criteria for Selection:
+1) Closeness to the theme.
+2) Originality.
+3) Responsiveness.
+4) Usefulness.
+
+Last date for submission: 15th April’ 2014
+			</p>
+		</div>
+		<div id="border">
+		</div>
+		<div id="right">
+			<p>This is 2nd right</p>
+		</div>
+	</div>
+</div>
+</html>
