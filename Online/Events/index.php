c766a484db8aa35e1f15fde99115bb1231a7b56f
archangel <adi.shr09@gmail.com>
2014-04-13 04:02:23 +0530

    adds

--- Online/Events/index.php
+++ Online/Events/index.php
@@ -6,6 +6,15 @@
 		<title>BVPIEEE | Passion To Perfection</title>
 		<link rel="stylesheet" href="css/body.css" type="text/css">
 	</head>
+<body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <div id="wrapper" >
 	<div id="container">
 		<div id="left">
@@ -15,8 +24,10 @@
 		<div id="border">
 		</div>
 		<div id="right">
-			<p>This is 2nd right</p>
+			
 		</div>
 	</div>
 </div>
+<div class="fb-login-button" data-max-rows="1" data-size="medium" data-show-faces="false" data-auto-logout-link="false"></div>
+</body>
 </html>
