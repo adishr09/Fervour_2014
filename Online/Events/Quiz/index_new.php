4a24ca0f1223ab1b1d17e74d46cacb566d2f9074
archangel <adi.shr09@gmail.com>
2014-04-14 14:41:59 +0530

    new

--- Online/Events/Quiz/index_new.php
+++ Online/Events/Quiz/index_new.php
@@ -8,6 +8,31 @@
 	</head>
 <body>
 <script>setTimeout(function(){window.location.href='2nd_homepage/home_page.html'},1200000);</script>
+			<div id="wrapper">
+				<div id="myTimer"></div>
+				<button type="button" id="myBtn" class="btnDisable" disabled onclick="parent.location=''">Time Until quiz finishes</button>
+			</div>
+		<script>
+			var sec = 1200;
+			var myTimer = document.getElementById('myTimer');
+			var myBtn = document.getElementById('myBtn');
+			window.onload = countDown;
+			function countDown() {
+				if (sec < 10) {
+					myTimer.innerHTML = "0" + sec;
+				} else {
+					myTimer.innerHTML = sec;
+				}
+				if (sec <= 0) {
+					$("#myBtn").removeAttr("disabled");
+					$("#myBtn").removeClass().addClass("btnEnable");
+					$("#myTimer").fadeTo(2500, 0);
+					myBtn.innerHTML = "Quiz is Over!!";
+				return;
+			}
+			sec -= 1;
+			window.setTimeout(countDown, 1000);
+		}</script>
 <div id="wrapper" >
 	<div id="container">
 		<p>Hello Iframe</p>
