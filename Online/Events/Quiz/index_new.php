f4d332ac738fd1fa91e737299b722eda61172268
archangel <adi.shr09@gmail.com>
2014-04-14 14:20:55 +0530

    quiz

---
+++ Online/Events/Quiz/index_new.php
@@ -0,0 +1,17 @@
+<html style="overflow-x:hidden;">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1"> 
+		<title>BVPIEEE | Passion To Perfection</title>
+		<link rel="stylesheet" href="css/body.css" type="text/css">
+	</head>
+<body>
+<script>setTimeout(function(){window.location.href='2nd_homepage/home_page.html'},1200000);</script>
+<div id="wrapper" >
+	<div id="container">
+		<p>Hello Iframe</p>
+	</div>
+</div>
+</body>
+</html>
