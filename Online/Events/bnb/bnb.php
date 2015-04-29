a823ad1efef8cc49f2934064b18af8b13985e5cf
Priyansh Singh <priyanshsingh@outlook.com>
2015-04-29 12:19:27 +0530

    edits and stuff

--- Online/Events/bnb/bnb.php
+++ Online/Events/bnb/bnb.php
@@ -12,7 +12,7 @@ $p_pnb = 2.5;
 $credits = 1000; 
 	
 
-
+//PUTTING USER DATA IN SESSIONS 
 $_SESSION['$p_dlf'] = $p_dlf;
 $_SESSION['$p_itc'] = $p_itc;
 $_SESSION['$p_airtel'] = $p_airtel;
@@ -20,12 +20,13 @@ $_SESSION['$p_dabur'] = $p_dabur;
 $_SESSION['$p_maruti'] = $p_maruti;
 $_SESSION['$p_ongc'] = $p_ongc;
 $_SESSION['$p_pnb'] = $p_pnb;
-
+// GETING AND STORING USER DATA
 mysql_connect('mysql1.freehosting.com', 'bvpieeec_lol', 'hdb32wb98') or die("<br/>error");
 		mysql_select_db('bvpieeec_delhibvce') or die("<br>DB_error");
 		$q="SELECT * FROM `bnb` WHERE email='".$uname."'";
 $q_run=mysql_query($q) or die("<br/>error_run");
 $q_row=mysql_fetch_assoc($q_run);
+
 $a=$q_row['creds'];
 $s=$q_row['dlf'];
 $d=$q_row['itc'];
@@ -34,7 +35,7 @@ $g=$q_row['dabur'];
 $h=$q_row['maruti'];
 $j=$q_row['ongc'];
 $k=$q_row['pnb'];
-
+// STORING USER DATA IN SESSION 
 $_SESSION['u_dlf'] = $s;
 $_SESSION['u_itc'] = $d;
 $_SESSION['u_airtel'] = $f;
